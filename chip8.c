57b13b3d14f8d7700351414c98fa2f683fac3618
l-armstrong <lamone.armstrong@gmail.com>
2026-01-04 00:37:55 -0500

    feat: add the ability to open a chip8 ROM and dump the byes to the stdio.

--- chip8.c
+++ chip8.c
@@ -1,13 +1,15 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct memory_t {
     uint8_t map[4096];
 } memory_t;
 
-typedef struct stack_t {
+typedef struct stack16_t {
     uint16_t data[16]; // used to store the address that the interpreter should return to
     uint8_t  sp;
-} stack_t;
+} stack16_t;
 
 typedef struct cpu_t {
     uint8_t  V[16]; // 0xVF // used as a flag by some instructions
@@ -30,13 +32,55 @@ typedef struct display_t {
 typedef struct chip8_t {
     memory_t    mem;
     cpu_t       cpu;
-    stack_t     stack;
+    stack16_t   stack;
     display_t   display;
     keyboard_t  keyboard;
     uint8_t     delay_timer;
     uint8_t     sound_timer;
 } chip8_t;
 
-int main() {
-     
+// dispatch table
+
+void *xmalloc(size_t size) {
+    void *ptr = malloc(size);
+    if (ptr == NULL) {
+        fprintf(stderr, "Out of memory allocating %zu bytes\n", size);
+        exit(1);
+    }
+    return ptr;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <program.ch8>\n", argv[0]);
+        return 1;
+    }
+
+    /* Read the program into memory */
+    FILE *fp = fopen(argv[1], "rb");
+    if (fp == NULL) {
+        perror("Opening Chip8 program");
+        return 1;
+    }
+    /* Get file size */
+    fseek(fp, 0, SEEK_END);
+    long file_size = ftell(fp);
+    // Set cursor back to beginning
+    fseek(fp,0,SEEK_SET);
+
+    /* Read in chip8 program */
+    uint8_t *chip8_program = xmalloc(file_size+1);
+    size_t read = fread(chip8_program, 1, file_size, fp);
+
+    if (read != file_size) {
+        fprintf(stderr, "Failed to read full ROM.\n");
+        exit(1);
+    }
+    chip8_program[file_size] = 0;
+    fclose(fp);
+
+    for (int i = 0; i < file_size; i++) {
+        printf("%02X\n", chip8_program[i]);
+    }
+    return 0;
 }
